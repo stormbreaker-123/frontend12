168d69e91a006e012faf6bdbeb8589ddce8cb91d
stormbreaker-123 <sayantanpaul631@gmail.com>
2025-03-14 16:48:30 +0530

    ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -18,7 +18,7 @@ const ContactForm = () => {
     const formData = { name, email, message };
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://backendpf-mlez.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
